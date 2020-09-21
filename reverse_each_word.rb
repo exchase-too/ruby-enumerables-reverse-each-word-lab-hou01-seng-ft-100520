9936042b2c4a251f3cbc091a5928353575eb697f
Eric Smith <eric.c.smith@sbcglobal.net>
2020-09-21 17:41:41 +0000

    Automatically backed up by Learn

--- reverse_each_word.rb
+++ reverse_each_word.rb
@@ -1,9 +1,12 @@
+require 'pry'
+
 def reverse_each_word(sentence)
 
 new_Array = sentence.split
 reverse_array = []
 
 new_Array.collect {|v| reverse_array << v.reverse}
+binding.pry
 
 reverse_string = reverse_array.join(' ')
 end
